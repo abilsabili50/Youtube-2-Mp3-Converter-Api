8ffdda786237820d5d359c3f738fc8718a6ab8ac
Abilsabili <abilsabili50@gmail.com>
2022-11-16 00:57:26 +0700

    add do..while statement when apis processing the data

--- app.js
+++ app.js
@@ -10,36 +10,49 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.post("/", async (req, res) => {
-	const { youtubeUrl } = req.body;
-	let videoId;
-	if (youtubeUrl.includes("youtube.com/watch?v=")) {
-		videoId = youtubeUrl.split("v=")[1].split("&")[0];
-	} else if (youtubeUrl.includes("youtu.be/")) {
-		videoId = youtubeUrl.split("youtu.be/")[1];
-	}
 	try {
-		const fetchApi = await fetch(url + videoId, {
-			method: "GET",
-			headers: {
-				"X-RapidAPI-Key": process.env.API_KEY,
-				"X-RapidAPI-Host": process.env.API_HOST,
-			},
-		});
-
-		if (!fetchApi)
-			return res
-				.status(400)
-				.send({ status: "fail", message: "Error while converting the url" });
-
-		const fetchResponse = await fetchApi.json();
-
-		if (fetchResponse.status === "fail") {
-			return res
-				.status(404)
-				.send({ status: "fail", message: fetchResponse.msg });
+		const { youtubeUrl } = req.body;
+		let videoId;
+		if (youtubeUrl.includes("youtube.com/watch?v=")) {
+			videoId = youtubeUrl.split("v=")[1].split("&")[0];
+		} else if (youtubeUrl.includes("youtu.be/")) {
+			videoId = youtubeUrl.split("youtu.be/")[1];
 		}
+		let proses = true;
+		do {
+			const fetchApi = await fetch(url + videoId, {
+				method: "GET",
+				headers: {
+					"X-RapidAPI-Key": process.env.API_KEY,
+					"X-RapidAPI-Host": process.env.API_HOST,
+				},
+			});
 
-		return res.send({ status: "success", data: fetchResponse });
+			if (!fetchApi) {
+				proses = false;
+				return res
+					.status(400)
+					.send({ status: "fail", message: "Error while converting the url" });
+			}
+
+			const fetchResponse = await fetchApi.json();
+
+			if (fetchResponse.status === "fail") {
+				proses = false;
+				return res
+					.status(404)
+					.send({ status: "fail", message: fetchResponse.msg });
+			}
+
+			if (fetchResponse.status == "processing") {
+				continue;
+			}
+
+			if (fetchResponse.status == "ok") {
+				proses = false;
+				return res.send({ status: "success", data: fetchResponse });
+			}
+		} while (proses);
 	} catch (error) {
 		res.status(500).send({ status: "fail", message: error.message });
 	}
